cba94f752faaf4907e5e7bcb4748335a4b70b032
Mahmudulla Hassan <mhassan@miners.utep.edu>
2022-07-08 10:52:37 -0700

    allow localhost

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '^lw0)9982b8nt^^-t34967jnl(n7iojl!q+!5xojw9mr6u0h9f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    'localhost'
+    ]
 
 
 # Application definition
